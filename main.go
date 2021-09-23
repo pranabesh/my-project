b6c70e36a72c625f17d8d2c24d4ffd7224b30775
pranabesh <mail4pranabesh@gmail.com>
2021-09-23 11:46:15 -0700

    change 23

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - update1"}
+		res := &response{Message: "Hello World - update23"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
